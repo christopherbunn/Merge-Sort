fb31a99f8d0022000044c8a4d0199751a3ef392a
Chris <chris.l.bunn@gmail.com>
2017-02-01 00:44:30 -0500

    Filed into directories. Must run test executable.

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -1,9 +1,9 @@
 # CMake generated Testfile for 
-# Source directory: /Users/chris/Merge-Sort
-# Build directory: /Users/chris/Merge-Sort/build
+# Source directory: /home/chris/Documents/Merge-Sort
+# Build directory: /home/chris/Documents/Merge-Sort/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
-subdirs("lib")
+subdirs(src)
+subdirs(test)
+subdirs(lib)
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/chris/Merge-Sort
+# Install script for directory: /home/chris/Documents/Merge-Sort
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,11 +27,16 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/Users/chris/Merge-Sort/build/src/cmake_install.cmake")
-  include("/Users/chris/Merge-Sort/build/test/cmake_install.cmake")
-  include("/Users/chris/Merge-Sort/build/lib/cmake_install.cmake")
+  include("/home/chris/Documents/Merge-Sort/build/src/cmake_install.cmake")
+  include("/home/chris/Documents/Merge-Sort/build/test/cmake_install.cmake")
+  include("/home/chris/Documents/Merge-Sort/build/lib/cmake_install.cmake")
 
 endif()
 
@@ -43,5 +48,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/chris/Merge-Sort/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/chris/Documents/Merge-Sort/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
